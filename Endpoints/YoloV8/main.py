eb939ca76e195a21d6c89841683aeacc6c01c107
PewterZz <Peterns1609@gmail.com>
2023-12-29 20:10:28 +0700

    extra stuff

--- Endpoints/YoloV8/main.py
+++ Endpoints/YoloV8/main.py
@@ -1,7 +1,9 @@
 import json
+import time
 import requests
 from PIL import Image, ImageDraw
 
+start_time = time.time()
 url = "https://api.ultralytics.com/v1/predict/1GCDYisYuPV1rQ100wE4"
 headers = {"x-api-key": "76fc625b7251b36ad152cb916446b3bf555672d444"}
 data = {"size": 640, "confidence": 0.25, "iou": 0.45}
@@ -13,6 +15,12 @@ with open(image_path, "rb") as f:
 response.raise_for_status()
 
 response_data = response.json()
+end_time = time.time()
+
+size_of_data = len(response_data) + (1024 * 500)
+time_taken = end_time - start_time
+
+bandwidth = size_of_data / time_taken  
 
 image = Image.open(image_path)
 
@@ -24,4 +32,5 @@ for item in response_data["data"]:
     draw.rectangle([x1, y1, x2, y2], outline="red", width=2)
     print("name: ", item['name'])
 
+print(f"Bandwidth: {bandwidth} bytes/second")
 image.show()
